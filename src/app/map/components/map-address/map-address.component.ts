27a2a0bc32e556b06e3aaffc7aae0271482a7f65
Franklin Parker <fparker@deliveryman.com>
2018-07-23 13:00:29 -0400

    map service

--- src/app/map/components/map-address/map-address.component.ts
+++ src/app/map/components/map-address/map-address.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MapService} from '../../service/map.service';
 
 @Component({
   selector: 'app-map-address',
@@ -6,11 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./map-address.component.scss']
 })
 export class MapAddressComponent implements OnInit {
-  street: string = '140 South St';
-  city: string = 'Morristown';
-  constructor() { }
+  street = '140 South St';
+  city = 'Morristown';
+
+  constructor(private mapService: MapService) {
+  }
 
   ngOnInit() {
+  }
+
+  onMapLocation() {
+    const location = this.city + ', ' + this.street;
+    alert(location);
+    this.mapService.geocodeLocation(location)
+      .subscribe((coord) => {
+          console.log('got coord', coord);
+        },
+        (err) => {
+          alert(err);
+        });
   }
 
 }
--- src/app/map/map.module.ts
+++ src/app/map/map.module.ts
@@ -4,11 +4,15 @@ import {MapHomeComponent} from './components/map-home/map-home.component';
 import {MapAddressComponent} from './components/map-address/map-address.component';
 import {MapComponent} from './components/map/map.component';
 import {FormsModule} from '@angular/forms';
+import {AgmCoreModule} from '@agm/core';
 
 @NgModule({
   imports: [
     CommonModule,
-    FormsModule
+    FormsModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'hide'
+    })
   ],
   declarations: [
     MapHomeComponent,
